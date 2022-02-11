f2a371eb0dc0aaa3b33040adce497aa905f29270
ColinJr <colindetwillerjr@gmail.com>
2022-02-10 21:38:07 -0500

    Added interactivity

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    [SerializeField] float steerSpeed = 1f;
+    [SerializeField] float driveSpeed = 0.1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +16,9 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, 0.1f);
-        transform.Translate(0, 0.01f, 0);
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float gasPedal = Input.GetAxis("Vertical") * driveSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, gasPedal, 0);
     }
 }
