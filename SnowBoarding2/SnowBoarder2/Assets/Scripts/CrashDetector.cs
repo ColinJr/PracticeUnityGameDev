eb6593cead30f098e7aa619701ad4a9407c94615
ColinJr <colindetwillerjr@gmail.com>
2022-07-12 14:58:47 -0400

    Reload level on Win or Death

--- SnowBoarding2/SnowBoarder2/Assets/Scripts/CrashDetector.cs
+++ SnowBoarding2/SnowBoarder2/Assets/Scripts/CrashDetector.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class CrashDetector : MonoBehaviour
         if (other.tag == "Terrain" || other.tag == "Hazard")
         {
             Debug.Log("You DIED!");
+            SceneManager.LoadScene(0);
         }
     }
 }
--- SnowBoarding2/SnowBoarder2/Assets/Scripts/LevelComplete.cs
+++ SnowBoarding2/SnowBoarder2/Assets/Scripts/LevelComplete.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class LevelComplete : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player")
         {
-        Debug.Log("You FINISHED!");
+            Debug.Log("You FINISHED!");
+            SceneManager.LoadScene(0);
         }
     }
 }
